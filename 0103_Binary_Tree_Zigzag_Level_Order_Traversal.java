dd617398415be28ec4d432b892f66ddc504f7f50
kukkisandhuproton <131423888+kukkisandhuproton@users.noreply.github.com>
2023-05-19 19:42:24 -0400

    Create 0103_Binary_Tree_Zigzag_Level_Order_Traversal.java

---
+++ 0103_Binary_Tree_Zigzag_Level_Order_Traversal.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean flag = false;
+        while(!queue.isEmpty()){
+            int level = queue.size();
+            ArrayList<Integer> sublist = new ArrayList<>();
+            for(int i = 0;i<level;i++){
+                if(queue.peek().left!=null){
+                    queue.offer(queue.peek().left);
+                }
+                if(queue.peek().right!=null){
+                    queue.offer(queue.peek().right);
+                }
+                sublist.add(queue.poll().val);
+            }
+            if(flag){
+            Collections.reverse(sublist);
+            }
+            result.add(sublist);
+            flag=!flag;
+        }
+        return result;
+    }
+}
